82bbb5c048f5f3c097c79c4730ca23c6b378eea7
ryhan000 <rayhan1ahamed@gmail.com>
2018-05-24 23:37:35 +0600

    updated font size

--- src/main/java/heroku_demo/controller/HomeController.java
+++ src/main/java/heroku_demo/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
  
-	@GetMapping(value = {"/"})
+	@GetMapping(value = {"/","index"})
 	public String openHomePage() {
 		
 		return "index";
